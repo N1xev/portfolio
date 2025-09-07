c12efc7d8e9223b5ac8e15450d3ca4de542d84a7
Alaa Elsamouly <alasamouly@gmail.com>
2025-09-07 12:15:58 +0000

    Revert the welcome text dynamic logic and change it with svg and ${} it

--- src/components/outputs/welcome.tsx
+++ src/components/outputs/welcome.tsx
@@ -1,29 +1,18 @@
-import { useEffect, useState } from 'react';
-import { welcomeText, welcomeTextMobile } from '@/lib/information';
-import { useIsMobile } from '@/hooks/use-mobile';
+import { useEffect } from 'react';
+import { welcomeText } from '@/lib/information';
 
 interface WelcomeOutputProps {
   onComplete?: () => void;
 }
 
 export function WelcomeOutput({ onComplete }: WelcomeOutputProps) {
-  const isMobile = useIsMobile();
-  const [text, setText] = useState(isMobile ? welcomeTextMobile : welcomeText);
-
-  useEffect(() => {
-    setText(isMobile ? welcomeTextMobile : welcomeText);
-  }, [isMobile]);
-
   useEffect(() => {
     onComplete?.();
   }, [onComplete]);
 
   return (
     <div
-      className="font-code leading-[18px]"
-      style={{ whiteSpace: 'pre-wrap' }}
-    >
-      {text}
-    </div>
+      dangerouslySetInnerHTML={{ __html: welcomeText }}
+    />
   );
 }
