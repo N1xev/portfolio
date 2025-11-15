7c2e147f0c738d78a0e92a6be897bb59ed1341eb
N1xev <alasamouly@gmail.com>
2025-11-15 02:06:29 +0200

    update(nuxtConfig): added missing nuxt config stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,42 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/hints',
-    '@nuxt/image',
-    '@nuxt/ui'
-  ]
-})
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/hints",
+    "@nuxt/image",
+    "@nuxt/ui",
+    "@nuxtjs/color-mode",
+  ],
+  css: ["~/assets/css/main.css"],
+
+  content: {
+    build: {
+      markdown: {
+        highlight: {
+          langs: ["lua"],
+        },
+      },
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+app: {
+    head: {
+      title: 'Eng.Samouly', // default fallback title
+      htmlAttrs: {
+        lang: 'en',
+      },
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/samouly.png' },
+      ],
+    },
+  },
+});
