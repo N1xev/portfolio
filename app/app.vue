f7425823703a5b952ed9ae1d5ea9fe3ae5f2a455
N1xev <alasamouly@gmail.com>
2025-11-15 02:13:12 +0200

    fix(entryPoint): fixed app.vue after the accidental stashing

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <HeaderComp />
+    <UMain>
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </UMain>
+    <FooterComp />
+  </UApp>
 </template>
+
+<script setup lang="ts">
+
+const appConfig = useAppConfig()
+
+updateAppConfig(appConfig)
+
+</script>
