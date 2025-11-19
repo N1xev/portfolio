30e00669f3411f6aee3a0253a2b2b193beb1e6b0
N1xev <alasamouly@gmail.com>
2025-11-19 20:55:12 +0200

    Added content preview for nuxt studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,6 +22,9 @@ export default defineNuxtConfig({
         },
       },
     },
+      preview: {
+          api: 'https://api.nuxt.studio'
+      },
   },
   vite: {
     plugins: [tailwindcss()],
