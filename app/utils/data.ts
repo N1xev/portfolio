d9191780507ada2236b9e2d6932211c48f7e4af9
N1xev <alasamouly@gmail.com>
2025-11-20 01:16:36 +0200

    ]updated my projects data

--- app/utils/data.ts
+++ app/utils/data.ts
@@ -14,44 +14,60 @@ export const projects = [
         title: "This Portfolio",
         description: "A simple portfolio built with Nuxt 4, Tailwind, and Bun.",
         image: "/portfolio-preview.png",
-        tech: ["Nuxt 4", "tailwindcss", "Bun", "TypeScript", "vue"],
+        tech: ["NuxtJS", "tailwindcss", "Bun", "TypeScript", "Vue"],
         links: {
             github: "https://github.com/N1xev/portfolio",
             demo: "https://samouly.nuxt.dev/",
         },
     },
     {
-        title: "Llamanager",
-        description: "A TUI file manager like yazi written in Go.",
-        image: "/llamanager.png",
-        tech: ["Go", "BubbleTea", "lipgloss", "bubbles"],
-        links: {github: "https://github.com/N1xev/go-auth", demo: null},
-    },
-    {
-        title: "HyprLuna Shell",
-        description: "Arch linux setup for a fully-featured linux desktop.",
-        image: "/hyprluna.png",
-        tech: ["Arch Linux", "Hyprland", "Bash", "Ags", "JavaScript", "CSS", "SCSS"],
+        title: "CharmBracelet documentation",
+        description: "An unofficial charmbracelet documentation for bubbletea and lipglos etc..",
+        image: "/charm-docs.png",
+        tech: ["NextJS", "React", "TypeScript", "FumaDocs", "MDX"],
         links: {
-            github: "https://github.com/Lunaris-Project/HyprLuna",
+            github: "https://github.com/N1xev/store-app",
             demo: null,
         },
     },
     {
-        title: "This Portfolio",
-        description: "A simple portfolio built with Nuxt 4, Tailwind, and Bun.",
-        image: "/portfolio-preview.png",
-        tech: ["Nuxt 4", "tailwindcss", "Bun", "TypeScript", "vue"],
-        links: {
-            github: "https://github.com/N1xev/portfolio",
-            demo: "https://samouly.nuxt.dev/",
-        },
-    },
-    {
         title: "Llamanager",
         description: "A TUI file manager like yazi written in Go.",
         image: "/llamanager.png",
         tech: ["Go", "BubbleTea", "lipgloss", "bubbles"],
-        links: {github: "https://github.com/N1xev/go-auth", demo: null},
+        links: {
+            github: "https://github.com/N1xev/go-auth",
+            demo: null
+        },
+    },
+    {
+        title: "Samouly Store",
+        description: "A fullstack store web application for fashion artist.",
+        image: "/samouly-store.png",
+        tech: ["NextJS", "React", "TypeScript", "Prisma", "NeonPostgres"],
+        links: {
+            github: "https://github.com/N1xev/store-app",
+            demo: "https://samouly-store.vercel.app/",
+        },
+    },
+    {
+        title: "Samouly Flake",
+        description: "My NixOS setup and dotfiles with home-manager and nix flakes.",
+        image: "/samouly-flake.jpg",
+        tech: ["Nix", "Bash", "kdl", "conf"],
+        links: {
+            github: "https://github.com/N1xev/samouly-flake",
+            demo: null,
+        },
+    },
+    {
+        title: "Nixev Land",
+        description: "this is my first own Arch linux dotfiles with hyprland!",
+        image: "/nixev-land.png",
+        tech: ["HyprLand", "WayBar", "Bash", "TypeScript", "bun"],
+        links: {
+            github: "https://github.com/N1xev/Nixev-Land",
+            demo: null,
+        },
     }
 ];
